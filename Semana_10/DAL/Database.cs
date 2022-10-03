b4ab2dc827bd6f0b9563063b8891ee7186844c28
Darien <Darien@DESKTOP-ODLUPBB>
2022-10-03 11:54:50 -0600

    Agregar archivos de proyecto.

---
+++ Semana_10/DAL/Database.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using Semana_10.Properties;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace Semana_10.DAL
+{
+    public class Database
+    {
+        public static string getStrConnection()
+        {
+            return Settings.Default.empleadosConnectionString;
+        }
+
+        public SqlConnection GetConnection()
+        {
+            SqlConnection Con = new SqlConnection(getStrConnection());
+            return Con;
+        }
+    }
+}
